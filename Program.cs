12dfb640033c710f662a3b856c8e7f014c520fb9
Johan Holm <johan_holm@live.com>
2022-10-20 11:23:01 +0200

    Fixed a divide by zero error handeling + alot small fixes

--- Program.cs
+++ Program.cs
@@ -11,208 +11,164 @@
             bool programRunning = true;
             string saveAnswer = "";
 
+            //Creat an while-loop for the first menu. it can go as long as the user choose to quit.
             while (programRunning)
             {
-                Console.Clear();               
-                // Välkomnande meddelande
+                Console.Clear(); //Clears the console window             
                 Console.WriteLine("\n\n\n\n\t\tWelcome to the calculator!\n");
                 Console.WriteLine("\t\tPress 1 for count with the calculator");
-                //Vilken typ av uträkning kanske borde finnas i menyvalet???
                 Console.WriteLine("\t\tPress 2 to see old results");
-                Console.WriteLine("\t\tPress 3 for quit the program (answer/s are going to be delete)");
-                Console.WriteLine();
+                Console.WriteLine("\t\tPress 3 for quit the program (answer/s are going to be delete)\n");
+                Console.Write("\t\t");
 
-
+                //Handeling errors if the user dosnt write 1, 2 or 3.
                 Int32.TryParse(Console.ReadLine(), out int menyVal);
-                if (menyVal == 1)
-                {
-                    Console.Clear();                    
-                    Console.WriteLine("\n\n\n\n\t\tThis calculator can do a calculations with two numbers.");
-                    double nr1 = 0;
-                    double nr2 = 0;
-                    double answer = 0;                    
-                    bool loopRunning = true;
-                    while (loopRunning)
-                    {
-                        try
-                        { //Börja med siffra, sen vad jag vill räkna med, sen siffra, fortsätt till det trycks på likamed och spara.
-
-                            Console.Clear();
-                            Console.Write("\n\n\n\n\t\tWrite your first number: ");
-                            nr1 = double.Parse(Console.ReadLine());
-                            loopRunning = false;
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.Clear();                            
-                            Console.WriteLine("\n\n\n\n\t\tPlease, write a number (press any key)\n");
-                            Console.WriteLine("Error message:");
-                            Console.WriteLine(ex);
-                            Console.ReadKey();
-                        }
-                    }
-                    loopRunning = true;
-                    while (loopRunning)
-                    {
-                        try
-                        { //Börja med siffra, sen vad jag vill räkna med, sen siffra, fortsätt till det trycks på likamed och spara.
-
-                            Console.Clear();                            
-                            Console.Write("\n\n\n\n\t\tWrite your second number: ");
-                            nr2 = double.Parse(Console.ReadLine());
-                            loopRunning = false;
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.Clear();
-                            Console.WriteLine("\n\n\n\n\t\tPlease, write a number (press any key)\n");
-                            Console.WriteLine("Error message:");
-                            Console.WriteLine(ex);
-                            Console.ReadKey();
-
-                        }
-                    }
-                    loopRunning = true;
-                    while (loopRunning)
-                    {
-                        char oper = '0';
-                        try
-                        { //Börja med siffra, sen vad jag vill räkna med, sen siffra, fortsätt till det trycks på likamed och spara.
-
-                            Console.Clear();                            
-                            Console.WriteLine("\n\n\n\n\t\tWrite an operand (+, -, /, *):");
-                            oper = char.Parse(Console.ReadLine());
-                            if (oper != '+' || oper != '-' || oper != '/' || oper != '*')
-                            {
-                                switch (oper)
-                                {
-                                    case '+':
-                                        answer = nr1 + nr2;
-                                        break;
-
-                                    case '-':
-                                        answer = nr1 - nr2;
-                                        break;
-
-                                    case '/':
-                                        answer = nr1 / nr2;
-                                        break;
-
-                                    case '*':
-                                        answer = nr1 * nr2;
-                                        break;
-
-                                    default:
-                                        //This message shows up if something else then 1, 2 or 3 rights when the menu is open.
-
-                                        break;
-
-                                }
-                                Console.Clear();
-                                Console.WriteLine("\n\n\n\n\t\tYour calculation are as follow: " + nr1 + oper + nr2 + "=" + answer);
-                                saveAnswer = "" + nr1 + oper + nr2 + "=" + answer;
-                                list.Add(saveAnswer);
-                                Console.WriteLine("\t\tYour answer is saved as " + saveAnswer + " and you can see it if you press 2 in the first menu.");
-                                Console.ReadKey();
-                                loopRunning = false;
-                            }
-                            else
-                                Console.WriteLine("\n\n\n\n\t\tPlease, write an operand (+, -, / or *).");
-
-                        }
-                       catch (Exception ex)
-                        {
-                            Console.Clear();
-                            Console.WriteLine("\n\n\n\n\t\tPlease, write an operand (+, -, / or *).");
-                            Console.WriteLine();
-                            Console.WriteLine(ex);
-                            //Console.ReadKey();
-
-                        }                  
-                    }
-                }
-                if (menyVal == 2)
-                {
-                    Console.Clear();
-                    //try
-                    //{
-                        for (int i =0; i<list.Count; i++)
-                        {
-                            Console.WriteLine("\n\t--------------------------------------------------------\n");
-                            Console.WriteLine("\t\tYour answers are as follow:");
-                            Console.Write("\t\tNr:" + (i + 1) + " ");
-                            Console.WriteLine(list[i]);
-                        }
-                    /*}
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("\n\n\n\n\t\tThere are no calculations saved yet. Please do some calculations!");
-                        Console.WriteLine(ex);
-                        Console.ReadKey();
-                    }
-                    */
-                    Console.WriteLine("\n\n\n\n\t\tPress any key to go back to the menu.");
-                    Console.ReadKey();
-                    continue;
-                }
-
-                if (menyVal == 3)
-                {
-                    Console.Clear ();
-                    Console.WriteLine();
-                    Console.WriteLine("\n\n\n\n\t\tThank you. Bye!");
-                    programRunning = false;
-                }
-
-                else if (saveAnswer == "")
-                {
-                    Console.WriteLine("\n\n\n\n\t\tYou need to right 1, 2 or 3.");
-                    Console.ReadKey();
-                }
-            }
-            //Created an method to have an menu in the menu
-            //It will return witch parameter to count whit
-            /*char Countwith();
-            {
-                //bool
-                //while 
-               
-                Console.WriteLine("Vad vill du räkna med?");
-                Console.WriteLine("Press 1 for count with Multiplication");
-                Console.WriteLine("Press 2 for count with Division");
-                Console.WriteLine("Press 3 for count with Addition");
-                Console.WriteLine("Press 4 for count with Subtraction");
-
-                Int32.TryParse(Console.ReadLine(), out int menyVal);
-
+                
                 switch (menyVal)
                 {
                     case 1:
-                        char multi = '*';
+                        Console.Clear();
+                        Console.WriteLine("\n\n\n\n\t\tThis calculator can do a calculations with two numbers (Press any key to go forward).");
+                        Console.ReadKey();
+                        double nr1 = 0;
+                        double nr2 = 0;
+                        double answer = 0;
+                        bool loopRunning = true;
+                        
+                        while (loopRunning)
+                        {
+                            try
+                            {
+                                Console.Clear();
+                                Console.Write("\n\n\n\n\t\tWrite your first number and then enter: ");
+                                nr1 = double.Parse(Console.ReadLine());
+                                loopRunning = false;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\n\n\n\n\t\tPlease, write a number (press any key)\n");
+                                Console.WriteLine("Error message:");
+                                Console.WriteLine(ex);
+                                Console.ReadKey();
+                            }
+                        }
+                        bool loop2Running = true;
+                        while (loop2Running)
+                        {
+                            try
+                            {
+                                Console.Clear();
+                                Console.Write("\n\n\n\n\t\tWrite your second number and then enter: ");
+                                nr2 = double.Parse(Console.ReadLine());
+                                loop2Running = false;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\n\n\n\n\t\tPlease, write a number (press any key)\n");
+                                Console.WriteLine("Error message:");
+                                Console.WriteLine(ex);
+                                Console.ReadKey();
+
+                            }
+                        }
+                        bool loop3Running = true;
+                        while (loop3Running)
+                        {
+                            char oper = '0';
+                            
+                                Console.Clear();
+                                Console.WriteLine("\n\n\n\n\t\tChoose an operand (+, -, /, *) to count with:");
+                                oper = char.Parse(Console.ReadLine());
+                                if (oper == '+' || oper == '-' || oper == '/' || oper == '*')
+                                {
+                                    switch (oper)
+                                    {
+                                        case '+':
+                                            answer = nr1 + nr2;
+                                            break;
+
+                                        case '-':
+                                            answer = nr1 - nr2;
+                                            break;
+
+                                        case '/':
+                                                answer = nr1 / nr2;
+                                                break;
+
+                                        case '*':
+                                            answer = nr1 * nr2;
+                                            break;
+
+                                        default:
+                                            Console.Clear();
+                                            Console.WriteLine("\n\n\n\n\t\tPlease, press any key and then write an operand (+, -, / or *).");
+                                            Console.ReadLine();
+                                            break;
+
+                                    }
+                                    if (nr2 == 0 && oper == '/')
+                                    {
+                                    Console.WriteLine("\t\tYou cant divide with 0.\n\t\tPlease try again.");
+                                    Console.ReadKey();
+                                    break;
+                                    }
+                                    else
+                                    {
+                                    Console.Clear();
+                                    Console.WriteLine("\n\n\n\n\t\tYour calculation are as follow: " + nr1 + oper + nr2 + "=" + answer);
+                                    saveAnswer = "" + nr1 + oper + nr2 + "=" + answer;
+                                    list.Add(saveAnswer);
+                                    Console.WriteLine("\t\tYour answer is saved as " + saveAnswer + " and you can see it if you press 2 in the first menu.");
+                                    Console.ReadKey();
+                                    loop3Running = false;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("\n\n\n\n\t\tPlease, press any key and then write an operand (+, -, / or *).");
+                                    Console.ReadLine();
+                                }                            
+                        }
                         break;
                     
-
                     case 2:
-                        char divi = '/';
+                        if (saveAnswer != "") //saveAnswer need to have something so this if-statment runs  
+                        {
+                            for (int i = 0; i < list.Count; i++)
+                            {
+                                Console.WriteLine("\t\t-------------------------");
+                                Console.Write("\t\tResult nr:" + (i + 1) + " ");
+                                Console.WriteLine(list[i]);
+                            }
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n\n\n\n\t\tThere are no calculation done yet. Please, do some calculations! " +
+                                "\n\t\t(Press any key to go back to the menu)");
+                            Console.ReadKey();
+                            continue; //put a continue so it gose back to the menu
+                        }
+                        Console.WriteLine("\n\t\tPress any key to go back to the menu.");
+                        Console.ReadKey();
                         break;
 
                     case 3:
-                        char add = '+';
+                        Console.Clear();
+                        Console.WriteLine();
+                        Console.WriteLine("\n\n\n\n\t\tThank you. Bye!");
+                        programRunning = false;
                         break;
-
-                    case 4:
-                        char sub = '-';
+                    
+                    default:
+                        Console.WriteLine("\n\n\n\n\t\tYou neeed to write 1, 2 or 3.");
+                        Console.ReadKey();
                         break;
-
-                        default:
-                        //This message shows up if something else then 1, 2, 3 or 4 rights when the menu is open.
-                        Console.WriteLine("You need to right 1, 2, 3 or 4.");
-                        break;
-
                 }
             }
-            */
-
 
             // En lista för att spara historik för räkningar
 
@@ -234,4 +190,5 @@
         }
     }
 }
+
     
