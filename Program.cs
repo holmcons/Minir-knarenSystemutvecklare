fcbd59dfbcaa004e4cfe9397be3f22dc40a6a0ab
Johan Holm <johan_holm@live.com>
2022-10-21 07:32:47 +0200

    Put a switch in a metod + some regular clean up in the code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MiniräknarenSystemutvecklare
+﻿using System.IO.Pipes;
+
+namespace MiniräknarenSystemutvecklare
 {
     internal class Program
     {
@@ -9,7 +11,10 @@
 
             //Creat a bool as an button so you can quit the program from the menu.
             bool programRunning = true;
-            string saveAnswer = "";
+            //string saveAnswer = "";
+            double nr1 = 0;
+            double nr2 = 0;
+            double answer = 0;
 
             //Creat an while-loop for the first menu. it can go as long as the user choose to quit.
             while (programRunning)
@@ -18,7 +23,7 @@
                 Console.WriteLine("\n\n\n\n\t\tWelcome to the calculator!\n");
                 Console.WriteLine("\t\tPress 1 for count with the calculator");
                 Console.WriteLine("\t\tPress 2 to see old results");
-                Console.WriteLine("\t\tPress 3 for quit the program (answer/s are going to be delete)\n");
+                Console.WriteLine("\t\tPress 3 for quit the program (answer/s are going to be deleted)\n");
                 Console.Write("\t\t");
 
                 //Handeling errors if the user dosnt write 1, 2 or 3.
@@ -28,11 +33,8 @@
                 {
                     case 1:
                         Console.Clear();
-                        Console.WriteLine("\n\n\n\n\t\tThis calculator can do a calculations with two numbers (Press any key to go forward).");
+                        Console.Write("\n\n\n\n\t\tThis calculator can do a calculations with two numbers (Press any key to go forward).");
                         Console.ReadKey();
-                        double nr1 = 0;
-                        double nr2 = 0;
-                        double answer = 0;
                         bool loopRunning = true;
                         
                         while (loopRunning)
@@ -76,46 +78,33 @@
                         bool loop3Running = true;
                         while (loop3Running)
                         {
-                            char oper = '0';
-                            
+                            try
+                            {
+                                string saveAnswer = "";
+                                char oper = '0';
                                 Console.Clear();
-                                Console.WriteLine("\n\n\n\n\t\tChoose an operand (+, -, /, *) to count with:");
+                                Console.Write("\n\n\n\n\t\tChoose an operand (+, -, /, *) to count with: ");
                                 oper = char.Parse(Console.ReadLine());
                                 if (oper == '+' || oper == '-' || oper == '/' || oper == '*')
                                 {
-                                    switch (oper)
-                                    {
-                                        case '+':
-                                            answer = nr1 + nr2;
-                                            break;
 
-                                        case '-':
-                                            answer = nr1 - nr2;
-                                            break;
-
-                                        case '/':
-                                                answer = nr1 / nr2;
-                                                break;
-
-                                        case '*':
-                                            answer = nr1 * nr2;
-                                            break;
-
-                                        default:
-                                            Console.Clear();
-                                            Console.WriteLine("\n\n\n\n\t\tPlease, press any key and then write an operand (+, -, / or *).");
-                                            Console.ReadLine();
-                                            break;
-
-                                    }
-                                    if (nr2 == 0 && oper == '/')
-                                    {
-                                    Console.WriteLine("\t\tYou cant divide with 0.\n\t\tPlease try again.");
+                                    InnerSwitch(nr1, nr2, oper);
+                                    
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("\n\n\n\n\t\tPlease, press any key and then write an operand (+, -, / or *).");
+                                    Console.ReadLine();
+                                }
+                                if (nr2 == 0 && oper == '/')
+                                {
+                                    Console.WriteLine("\t\tYou cant divide with 0 (zero).\n\t\tPlease try again.");
                                     Console.ReadKey();
                                     break;
-                                    }
-                                    else
-                                    {
+                                }
+                                else
+                                {
                                     Console.Clear();
                                     Console.WriteLine("\n\n\n\n\t\tYour calculation are as follow: " + nr1 + oper + nr2 + "=" + answer);
                                     saveAnswer = "" + nr1 + oper + nr2 + "=" + answer;
@@ -123,26 +112,32 @@
                                     Console.WriteLine("\t\tYour answer is saved as " + saveAnswer + " and you can see it if you press 2 in the first menu.");
                                     Console.ReadKey();
                                     loop3Running = false;
-                                    }
                                 }
-                                else
-                                {
-                                    Console.Clear();
-                                    Console.WriteLine("\n\n\n\n\t\tPlease, press any key and then write an operand (+, -, / or *).");
-                                    Console.ReadLine();
-                                }                            
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\n\n\n\n\t\tPlease, press any key and then write an operand (+, -, / or *).\n");
+                                Console.WriteLine("Error message:");
+                                Console.WriteLine(ex);
+                                Console.ReadKey();
+                            }
                         }
                         break;
                     
                     case 2:
-                        if (saveAnswer != "") //saveAnswer need to have something so this if-statment runs  
+                        if (list.Any()) //saveAnswer need to have something so this if-statment runs  
                         {
+                            Console.Clear();
+                            Console.WriteLine("\n\n\n\n\t\tYour results are as follow:\n");
                             for (int i = 0; i < list.Count; i++)
                             {
                                 Console.WriteLine("\t\t-------------------------");
-                                Console.Write("\t\tResult nr:" + (i + 1) + " ");
+                                Console.Write("\t\tNr:" + (i + 1) + " ");
                                 Console.WriteLine(list[i]);
                             }
+                            Console.WriteLine("\n\t\tPress any key to go back to the menu.");
+                            Console.ReadKey();
                         }
                         else
                         {
@@ -152,8 +147,6 @@
                             Console.ReadKey();
                             continue; //put a continue so it gose back to the menu
                         }
-                        Console.WriteLine("\n\t\tPress any key to go back to the menu.");
-                        Console.ReadKey();
                         break;
 
                     case 3:
@@ -169,25 +162,36 @@
                         break;
                 }
             }
+            double InnerSwitch(double nr1, double nr2, char oper)
+            {
+                switch (oper)
+                {
+                    case '+':
+                        answer = nr1 + nr2;
+                        return answer;
 
-            // En lista för att spara historik för räkningar
+                    case '-':
+                        answer = nr1 - nr2;
+                        return answer;
 
-            // Användaren matar in tal och matematiska operation
+                    case '/':
+                        answer = nr1 / nr2;
+                        return answer;
 
-            //OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+                    case '*':
+                        answer = nr1 * nr2;
+                        return answer;
 
-            // Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
-
-            // Lägga resultat till listan
-
-            //Visa resultat
-
-            //Fråga användaren om den vill visa tidigare resultat.
-
-            //Visa tidigare resultat
-
-            //Fråga användaren om den vill avsluta eller fortsätt
+                    /*default:
+                        Console.Clear();
+                        Console.WriteLine("\n\n\n\n\t\tPlease, press any key and then write an operand (+, -, / or *).");
+                        Console.ReadLine();
+                        break;*/
+                }
+                return answer;
+            }
         }
+           
     }
 }
 
